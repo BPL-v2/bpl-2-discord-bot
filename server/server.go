32184b5cb140e29cfa2e118ab5831e24c1f9dc32
Liberator <fabian.mueller77@gmail.com>
2026-07-29 11:43:46 +0200

    expose endpoint for server membership checks

--- server/server.go
+++ server/server.go
@@ -13,6 +13,28 @@ func SetRoutes(r *gin.Engine, session *discordgo.Session, bplClient *client.Clie
 	group := r.Group("/discord")
 	group.Handle("POST", ":guild_id/assign-roles", triggerRoleAssignmentHandler(session, bplClient))
 	group.Handle("GET", ":guild_id/members", getAllGuildMembers(session))
+	group.Handle("GET", ":guild_id/members/:discord_id", getMemberByDiscordID(session))
+}
+
+func getMemberByDiscordID(session *discordgo.Session) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		guildID := c.Param("guild_id")
+		if guildID == "" {
+			c.JSON(400, gin.H{"message": "missing guild_id"})
+			return
+		}
+		discordID := c.Param("discord_id")
+		if discordID == "" {
+			c.JSON(400, gin.H{"message": "missing discord_id"})
+			return
+		}
+		member, err := session.GuildMember(guildID, discordID)
+		if err != nil {
+			c.JSON(500, gin.H{"message": fmt.Sprintf("error getting member: %v", err)})
+			return
+		}
+		c.JSON(200, member)
+	}
 }
 
 func triggerRoleAssignmentHandler(session *discordgo.Session, bplClient *client.ClientWithResponses) gin.HandlerFunc {
